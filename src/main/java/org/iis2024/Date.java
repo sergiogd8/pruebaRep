b03d0a95226ec9e7cc85552c09b7d5740b4f1cb4
sergiogd8 <sergioglezd8@gmail.com>
2025-06-09 22:27:25 +0200

    Añadir cambios del colaborador

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -16,6 +16,7 @@ import java.text.SimpleDateFormat;
  * month, and year values cannot be changed.
  */
 public class Date {
+  private int variableAddedByCollaborator;
   private final int day;
   private final int month;
   private final int year;
